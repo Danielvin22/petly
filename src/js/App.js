271f4100c61bf505c713bda643cb871d75f10543
chris097 <chrisfidel.international@gmail.com>
2023-03-13 13:59:54 +0100

    new change

--- src/js/App.js
+++ src/js/App.js
@@ -1,42 +1,49 @@
-//  async function start() {
-//   const response = await fetch("https://dog.ceo/api/breeds/list/all")   
-//   const data =  await response.json()
-//   // createpetlist(data.message)
+ async function start() {
+  const response = await fetch("https://pets-v2.dev-apis.com/pets")   
+  const data =  await response.json()
+   createPetList(data.pets)
+  console.log(data)
 
-// }
+}
 
-// start()
+const createPetList = (pet) => { 
+  let petImage =`
+  <div class="cards-display">
+   ${pet.map(data_url => `
+      <div class="cards-content"> 
+        <img id="dog-card" class="card1" src=${data_url.images[0]} alt="">
+      
+        <div class="luna">
+          <div>
+            <h3 class="luna-h3">${data_url.name}</h3>
+            <h4 class="havenese">${data_url.breed}</h4>
+          </div>
+          <img class="skeleton-" src="./src/public/svg/dog.svg" alt="">
+        </div>
+      </div>
+
+      <div class="search-location">
+        <button class="view"><a hreidf="details.html">View Details</a></button>
+        <div class="seattle">
+          <img class="location-svg" src="./src/public/svg/location.svg" alt="">
+          <h4 class="seattle-wa">${data_url.city} ${data_url.state}</h4>
+        </div>
+      </div>
+    `)}
+  
+  </div>`
+
+  document.getElementById('cardsdisplay').innerHTML = petImage
+  
+
+}
+
+start()
 
 
 
-// function createPetList(petlist) {
-//   document.getElementById("dog-card")
-// }
 
 
-// fetch('https://pets-v2.dev-apis.com/pets').then((data)=>{
-//   // console.log(data);
-//   return data.json();
-// }).then((completedata)=>{
-//   console.log(completedata[2]);
-// })
 
-
-const div = document.getElementById('cardsDisplay')
-
-fetch('https://pets-v2.dev-apis.com/pets')
-  .then((response) => response.json())
-  .then((data) => {
-    let cardsDisplay = data
-    cardsDisplay.map(function(play){
-      let name = document.createElement('h2')
-      let email = document.createElement('h2')
-
-      name.innerHTML = `${play.brand}`
-      console.log('data');
-    
-
-    })
-
-  });
+ 
 
